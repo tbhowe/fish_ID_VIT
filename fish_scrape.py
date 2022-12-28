e17d35d5ecb91e87f3993f7d27b1fde83c76c425
Tim Howe <53109226+tbhowe@users.noreply.github.com>
2022-12-28 06:05:25 +0000

    Creates GetImages class - own code to scrape images from google based on a keyword, as old library wasn't working

--- fish_scrape.py
+++ fish_scrape.py
@@ -1,36 +1,91 @@
-from google_images_download import google_images_download
+#%%
 
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import os
+import time
 
-def download_fish_images(fish_type,prefix_string,number_of_images,chromedriver_path):
-    ''' Function scrapes google for images based on keyword
-        
-        input args:
-            fish_type (str)         - keyword of interest
-            prefix_string (str)     - additional keywords as prefix (eg. river name)
-            number_of_images (int)  - limit number of images to download
-            chromedriver path (str) - path to local chromedriver install
+class GetImages:
+
+    def __init__(self, keyword: str, chromedriver_path: str, opts=None): 
+        self.keyword=keyword
+        self.chromedriver_path='/Users/rrritalin/miniconda3/envs/zoopla/bin/chromedriver'
+        # self.chromedriver_path=chromedriver_path
+        self.base_url='https://images.google.com/'
+        download_path='images/'
+        self.driver =webdriver.Chrome(self.chromedriver_path)
+
+    def get_search_page(self):
+        '''Method to open the search page on google images '''
+        self.driver.get('https://images.google.com/')
+        self.driver.maximize_window()
+
+    def accept_cookies(self):
+        '''Method to accept the GFPR cookies on google images page '''
+        delay = 2
+        try:
+            WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[2]')))
+            print("cookiebox Ready!")
+            try:
+                accept_cookies_button = self.driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[2]') 
+                WebDriverWait(self.driver, delay).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[2]'))) 
+                self.driver.execute_script("arguments[0].click();", accept_cookies_button)
+                time.sleep(1)
+
+            except NoSuchElementException:
+                print("cookie button not present")
             
-        top n images are scraped from google and downloaded 
-        into path specified in "output_directory '''
+        except TimeoutException:
+            print("No GDPR cookie box appeared!")
+        
+    def enter_search(self):
+        '''method to enter search term into image search box'''
+        search_box=self.driver.find_element(by=By.XPATH, 
+                                            value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+        search_box.send_keys(self.keyword)
+        search_box.send_keys(Keys.ENTER)
+                                        
 
-    response = google_images_download.googleimagesdownload() 
-    arguments = {"keywords": fish_type,  
-                "prefix_keywords": prefix_string, 
-                "limit":number_of_images,
-                "print_urls":False,
-                'output_directory':'fish_images/', 
-                'chromedriver': chromedriver_path, 
-                'extract_metadata':False,
-                'type':'photo' ,
-                'size': 'medium',
-                'thumbnail_only':False} 
-                
-    response.download(arguments) 
 
 # script:
+
+fish_images=GetImages(keyword='perch', chromedriver_path='/Users/rrritalin/miniconda3/envs/zoopla/bin/chromedriver')
+fish_images.get_search_page()
+fish_images.accept_cookies()
+fish_images.enter_search()
+
+
+
+
+# driver.get('https://images.google.com/')
+# driver.maximize_window()
+# query='perch'
+# limit='50'
+# box = driver.find_element(By.XPATH, value='//*[@id="sbtc"]/div/div[2]/input')
+# box.send_keys(query)
+# box.send_keys(Keys.ENTER)
+
+# for image_number in range(1, limit):
     
-chromedriver_path='/Users/rrritalin/miniconda3/envs/zoopla/bin/chromedriver'
-number_of_images=50
-fish_type='perch'
-prefix_string=None
-download_fish_images(fish_type,prefix_string,number_of_images,chromedriver_path)
+#     try:
+ 
+#       # XPath of each image
+#         img = driver.find_element(By.XPATH, value=
+#                                     '//*[@id="islrg"]/div[1]/div[' +
+#                                     str(i) + ']/a[1]/div[1]/img')                             
+#         img.screenshot(download_path + 
+#                        query + '_' + str(image_number) + '.png')
+#         time.sleep(0.2)
+#     except:
+#         continue
+# driver.close()
+
+
+# %%
