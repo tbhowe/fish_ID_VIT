d49fbc1742a4d362f897e5abb5ad1e0dd32c28c1
Tim Howe <53109226+tbhowe@users.noreply.github.com>
2022-12-28 06:54:10 +0000

    adds code to name=main to loop through 10 fish types and get n images for each

--- fish_scrape.py
+++ fish_scrape.py
@@ -14,8 +14,9 @@ import time
 
 class GetImages:
 
-    def __init__(self, keyword: str, chromedriver_path: str, opts=None): 
+    def __init__(self, chromedriver_path: str, opts=None): 
         self.keyword=keyword
+        self.suffix=' fish'
         self.chromedriver_path='/Users/rrritalin/miniconda3/envs/zoopla/bin/chromedriver'
         # self.chromedriver_path=chromedriver_path
         self.base_url='https://images.google.com/'
@@ -49,62 +50,46 @@ class GetImages:
         '''method to enter search term into image search box'''
         search_box=self.driver.find_element(by=By.XPATH, 
                                             value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
-        search_box.send_keys(self.keyword)
+        search_box.send_keys(self.keyword + self.suffix)
         search_box.send_keys(Keys.ENTER)
     
-    def get_images(self,limit):
-        '''method to get top n images from keyword'''
+    def get_images(self, keyword: str, limit: int):
+        '''method to get top n images from keyword, where n is the value passed in limit argument'''
         if os.path.exists(self.download_path + self.keyword +'/' )==False:
             os.makedirs(self.download_path + self.keyword +'/' )
         
-        for image_number in range(1, limit):
+        for image_number in range(1, limit+1):
             try:
                 img = self.driver.find_element(By.XPATH, value=
                                             '//*[@id="islrg"]/div[1]/div[' +
-                                            str(image_number) + ']/a[1]/div[1]/img')                             
-                img.screenshot(self.download_path + 
-                            self.keyword + '/_' + str(image_number) + '.png')
+                                            str(image_number) + ']/a[1]/div[1]/img')  
+                time.sleep(0.1) 
+
+                img.screenshot( self.download_path + 
+                                self.keyword + '/img_' + str(image_number) + '.png')
+
                 time.sleep(0.2)
             except:
                 continue
 
+    def close_driver(self):
+        '''method for closing the driver'''
+        self.driver.close()
+
                                         
+'pike, perch, zander, chub, barbel, pollack, plaice, mackerel, gurnard, scad'
 
 
-# script:
-
-fish_images=GetImages(keyword='perch', chromedriver_path='/Users/rrritalin/miniconda3/envs/zoopla/bin/chromedriver')
-limit=10
-fish_images.get_search_page()
-fish_images.accept_cookies()
-fish_images.enter_search()
-fish_images.get_images(limit)
-
-
-
-
-# driver.get('https://images.google.com/')
-# driver.maximize_window()
-# query='perch'
-# limit='50'
-# box = driver.find_element(By.XPATH, value='//*[@id="sbtc"]/div/div[2]/input')
-# box.send_keys(query)
-# box.send_keys(Keys.ENTER)
-
-# for image_number in range(1, limit):
-    
-#     try:
- 
-#       # XPath of each image
-#         img = driver.find_element(By.XPATH, value=
-#                                     '//*[@id="islrg"]/div[1]/div[' +
-#                                     str(i) + ']/a[1]/div[1]/img')                             
-#         img.screenshot(download_path + 
-#                        query + '_' + str(image_number) + '.png')
-#         time.sleep(0.2)
-#     except:
-#         continue
-# driver.close()
-
+if __name__ == "__main__":
+    fish_types=['pike','perch','zander','carp','chub','barbel','mackerel','gurnard','scad','ballan']
+    limit=200
+    for fish in fish_types:
+        fish_images=GetImages( chromedriver_path='/Users/rrritalin/miniconda3/envs/zoopla/bin/chromedriver')
+        keyword=fish_types(fish)
+        fish_images.get_search_page()
+        fish_images.accept_cookies()
+        fish_images.enter_search()
+        fish_images.get_images(keyword,limit)
+        fish_images.close_driver()
 
 # %%
