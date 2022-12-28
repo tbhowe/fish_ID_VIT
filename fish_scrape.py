f868590b62a6f32fa9c5aa8b7dff6301e150d73d
Tim Howe <53109226+tbhowe@users.noreply.github.com>
2022-12-28 06:20:13 +0000

    Full working version of fish_scrape.py

--- fish_scrape.py
+++ fish_scrape.py
@@ -19,7 +19,7 @@ class GetImages:
         self.chromedriver_path='/Users/rrritalin/miniconda3/envs/zoopla/bin/chromedriver'
         # self.chromedriver_path=chromedriver_path
         self.base_url='https://images.google.com/'
-        download_path='images/'
+        self.download_path='images/'
         self.driver =webdriver.Chrome(self.chromedriver_path)
 
     def get_search_page(self):
@@ -51,15 +51,34 @@ class GetImages:
                                             value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
         search_box.send_keys(self.keyword)
         search_box.send_keys(Keys.ENTER)
+    
+    def get_images(self,limit):
+        '''method to get top n images from keyword'''
+        if os.path.exists(self.download_path + self.keyword +'/' )==False:
+            os.makedirs(self.download_path + self.keyword +'/' )
+        
+        for image_number in range(1, limit):
+            try:
+                img = self.driver.find_element(By.XPATH, value=
+                                            '//*[@id="islrg"]/div[1]/div[' +
+                                            str(image_number) + ']/a[1]/div[1]/img')                             
+                img.screenshot(self.download_path + 
+                            self.keyword + '/_' + str(image_number) + '.png')
+                time.sleep(0.2)
+            except:
+                continue
+
                                         
 
 
 # script:
 
 fish_images=GetImages(keyword='perch', chromedriver_path='/Users/rrritalin/miniconda3/envs/zoopla/bin/chromedriver')
+limit=10
 fish_images.get_search_page()
 fish_images.accept_cookies()
 fish_images.enter_search()
+fish_images.get_images(limit)
 
 
 
